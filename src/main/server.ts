3181c67d536f420f53cda0b690afbc64f2dd7bd1
Lucas Masayuki <lutamaribuchi@gmail.com>
2021-05-12 03:16:04 -0300

    Port

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,9 @@
 import App from './config/app'
 import loginRoutes from './routes/login-routes'
 
-const port = 3000 || parseInt(process.env.PORT, 10)
+const port = process.env.PORT || '3000'
 
 const routes = [loginRoutes]
 
-const app = new App(routes, port)
+const app = new App(routes, parseInt(port, 10))
 app.listen()
